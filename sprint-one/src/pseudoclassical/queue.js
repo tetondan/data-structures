cd6d1f3ad847fea8a2001561050bf31a0d33d3e6
tetondan <dfrehner1@gmail.com>
2015-11-19 15:36:46 -0800

    finished data structures sprint 1;

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,30 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.age = 0;
+  this.objects = {  };
 };
 
+Queue.prototype.size = function() {
+  return this.age >= 0 ? this.age : 0;
+}
 
+Queue.prototype.enqueue = function(val) {
+  if (this.age > 0) {
+    for (var key in this.objects) {
+      this.objects[key]++
+    }
+  }
+  this.objects[val] = 0;
+  this.age++;
+}
+
+Queue.prototype.dequeue = function() {
+  this.age--;
+  for (var key in this.objects) {
+    if (this.objects[key] === this.age) {
+      delete this.objects[key];
+      return key;
+    }
+  }
+};
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,22 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.length = 0;
 };
 
+Stack.prototype.size = function(){return this.length >= 0 ? this.length : 0;}
+
+Stack.prototype.push = function(val){
+  this[this.length] = val;
+  this.length++;
+}
+
+Stack.prototype.pop = function() {
+  if (this[this.length - 1]) {
+    var tempVal = this[this.length -1];
+    delete this[this.length-1];
+    this.length--;
+    return tempVal;
+  }
+}
 
