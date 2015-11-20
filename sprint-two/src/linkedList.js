da75b8a0068752ef73242d8788da3f79deacf60b
tetondan <dfrehner1@gmail.com>
2015-11-19 20:43:59 -0800

    finished linkedList project from sprint 2

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,14 +2,45 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
+  var container = [];
+  var count = 0;
 
   list.addToTail = function(value) {
+    if(this.tail === null){
+      this.tail = Node(value);
+      this.tail.next = (count +1)
+      this.head = this.tail;
+      container.push(this.tail.value);
+      this[count] = Node(value);
+      this[count].next = (count +1)
+      count++
+    } else {
+      this.tail = Node(value);
+      this.tail.next = (count +1)
+      this[count] = Node(value);
+      this[count].next = (count +1)
+      container.push(this.tail.value);
+      count++
+    }
   };
 
   list.removeHead = function() {
+    var headVal = this.head.value;
+    // remove head and make next value current head
+    var nextKey = this.head.next;
+    this.head = this[nextKey];
+    for(var i = 0; i < container.length; i++){
+      if(container[i] === headVal){
+        container.splice(i,1)
+      }
+    }
+    return headVal;
+
   };
 
   list.contains = function(target) {
+    return (_.indexOf(container, target) !== -1);
+
   };
 
   return list;
@@ -27,3 +58,4 @@ var Node = function(value) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
