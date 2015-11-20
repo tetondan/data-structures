3ea607d3bbe8288eabb80b23b50929f0b18f5583
tetondan <dfrehner1@gmail.com>
2015-11-20 11:54:39 -0800

    Completed Tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,8 +3,8 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];  // fix me
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
@@ -12,14 +12,18 @@ var treeMethods = {};
 
 treeMethods.addChild = function(value) {
   // your code here
-  newTree.children = null;  // fix me
+  this.children.push(Tree(value));  // fix me
 };
 
 treeMethods.contains = function(target) {
+  if (this.value === target) {
+      return true;
+  } else { 
+    for (var i = 0; i < this.children.length; i++) {
+      if(this.children[i].contains(target)){
+        return true;
+      }
+    }
+  } 
+  return false
 };
-
-
-
-/*
- * Complexity: What is the time complexity of the above functions?
- */
