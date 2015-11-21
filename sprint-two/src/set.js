3b8b1e53385300cf385e1330b186d60ddf12c2ed
tetondan <dfrehner1@gmail.com>
2015-11-20 21:59:19 -0800

    Updated Hash Table

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,21 +1,30 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
-  set._storage = undefined;
+  this._storage[item] = 0;
 };
 
 setPrototype.contains = function(item) {
+  for(var key in this._storage){
+    if(key === item){
+      return true;
+    }
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
+  delete this._storage[item]
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+
